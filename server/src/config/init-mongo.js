6473351cc632acc98d706dfadb1eab68d520307f
Ronaldo Scarpate <37854996+ronaldosc@users.noreply.github.com>
2026-01-18 03:08:33 -0300

    Refactor MongoDB initialization to use env object

--- server/src/config/init-mongo.js
+++ server/src/config/init-mongo.js
@@ -1,13 +1,18 @@
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
+const env = {
+  db: process.env.MONGO_INITDB_DATABASE,
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  password: process.env.MONGO_INITDB_ROOT_PASSWORD,
+};
+
 // Criar a database
-db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+db = db.getSiblingDB(env.db);
 
 // Criar o usuário e a senha de acesso ao banco de dados
 db.createUser({
-  user: process.env.MONGO_INITDB_ROOT_USERNAME,
-  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
-  roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }]
+  user: env.user,
+  pwd: env.password,
+  roles: [{ role: "readWrite", db: env.db }]
 });
